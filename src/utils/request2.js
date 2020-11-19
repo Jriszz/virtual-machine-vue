36f80969a74c8dafb74c435cc4ab6ff018d02e3f
Jris <zgfccc@126.com>
2020-11-19 17:04:04 +0800

    修改应用安装功能

--- src/utils/request2.js
+++ src/utils/request2.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
-import { hideLoading } from '@/utils/loading'
+import { hideLoading, showLoading } from '@/utils/loading'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.BASE_API2, // api 的 base_url
-  timeout: 6000 // request timeout
+  timeout: 60000 // request timeout
 
 })
 
@@ -15,6 +15,7 @@ service.interceptors.request.use(
     // do something before request is sent
     // config.headers['withCredentials'] =true
     config.headers['Content-type'] = 'application/json,text/plain,*/*'
+    showLoading()
     return config
   },
   error => {
